dce222307d0f005999c96f60eea304088524752a
Kyle Roberts <kyanro@gmail.com>
2022-05-05 16:30:50 -0500

    added PWA plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,19 @@ module.exports = {
         "path": "./src/pages/"
       },
       __key: "pages"
-    }]
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ]
 };
